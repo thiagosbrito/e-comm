bc220db763eaf243e4ce913c6cbc5b641411ab1d
Thiago Brito <thiago83brito@hotmail.com>
2023-11-21 09:12:34 +0100

    Feedback urls

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -9,8 +9,8 @@ export default function CartProvider({ children }: { children: ReactNode }) {
         mode="payment"
         cartMode="client-only"
         stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-        successUrl="http://localhost:3000/success"
-        cancelUrl="http://localhost:3000/error"
+        successUrl="https://e-comm-kappa.vercel.app/stripe/success"
+        cancelUrl="https://e-comm-kappa.vercel.app/stripe/error"
         currency="USD"
         billingAddressCollection={false}
         shouldPersist={true}
